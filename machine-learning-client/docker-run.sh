6eae5b126905c8de081a523ad318529de378940f
Michael Miao <ym2944@nyu.edu>
2026-04-11 17:52:30 -0400

    Mount the working directory

--- machine-learning-client/docker-run.sh
+++ machine-learning-client/docker-run.sh
@@ -1,6 +1,6 @@
 sudo docker build . -t ml-client
 
 clear
-
-sudo docker run -it --rm ml-client bash
+sudo docker run -it --rm -v $(pwd):/app -w /app ml-client bash
 sudo docker rmi ml-client
+clear
